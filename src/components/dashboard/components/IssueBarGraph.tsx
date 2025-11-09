c8e3abec7c08465bc1c6d36e4880aa3a03886e2e
CourtneyHisa <89618328+CourtneyHisa@users.noreply.github.com>
2025-11-08 17:06:06 -1000

    Added dummy data to Issue Bar Graph Display

---
+++ src/components/dashboard/components/IssueBarGraph.tsx
@@ -0,0 +1,77 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Chip from '@mui/material/Chip';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { useTheme } from '@mui/material/styles';
+
+export default function PageViewsBarChart() {
+  const theme = useTheme();
+  const colorPalette = [
+    (theme.vars || theme).palette.primary.dark,
+    (theme.vars || theme).palette.primary.main,
+    (theme.vars || theme).palette.primary.light,
+  ];
+  return (
+    <Card variant="outlined" sx={{ width: '100%' }}>
+      <CardContent>
+        <Typography component="h2" variant="subtitle2" gutterBottom>
+          Issues
+        </Typography>
+        <Stack sx={{ justifyContent: 'space-between' }}>
+          <Stack
+            direction="row"
+            sx={{
+              alignContent: { xs: 'center', sm: 'flex-start' },
+              alignItems: 'center',
+              gap: 1,
+            }}
+          >
+            <Typography variant="h4" component="p">
+              1.3M
+            </Typography>
+            <Chip size="small" color="error" label="-8%" />
+          </Stack>
+          <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+            IV & V Observations
+          </Typography>
+        </Stack>
+        <BarChart
+          borderRadius={8}
+          colors={colorPalette}
+          xAxis={[
+            {
+              scaleType: 'band',
+              categoryGapRatio: 0.5,
+              data: ['People', 'Process', 'Technology'],
+              height: 24,
+            },
+          ]}
+          yAxis={[{ width: 50 }]}
+          series={[
+            {
+              id: 'People',
+              label: 'Low',
+              data: [4, 2, 3],
+            },
+            {
+              id: 'Process',
+              label: 'Medium',
+              data: [10, 3, 8],
+            },
+            {
+              id: 'Technology',
+              label: 'High',
+              data: [3, 5, 1],
+            },
+          ]}
+          height={250}
+          margin={{ left: 0, right: 0, top: 20, bottom: 0 }}
+          grid={{ horizontal: true }}
+          hideLegend
+        />
+      </CardContent>
+    </Card>
+  );
+}
