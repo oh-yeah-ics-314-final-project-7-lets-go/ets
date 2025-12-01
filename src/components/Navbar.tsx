78806b9ce4ade3efcbef345cbc1a9b9b5c680740
kyler <143767857+kylersm@users.noreply.github.com>
2025-12-01 11:22:29 -1000

    add back button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,14 @@
 
 import { Role } from '@prisma/client';
 import { useSession } from 'next-auth/react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, Lock, PersonFill } from 'react-bootstrap-icons';
 import Image from 'next/image';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   const currentUser = session?.user?.email;
   const userWithRole = session?.user as { firstName: string; lastName: string; email: string; randomKey: string };
   const role = userWithRole?.randomKey as Role;
@@ -21,7 +22,22 @@ const NavBar: React.FC = () => {
       bg="light"
       expand="lg"
     >
-      <Container>
+      <Container className="px-0 mx-0">
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+        <div
+          onClick={() => router.back()}
+          className="flex ms-4 me-3"
+          title="Click to navigate to the previous page."
+          style={{
+            cursor: 'pointer',
+            textShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+          }}
+        >
+          <svg className="my-auto" height={20} width={20} viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+            <line strokeLinecap="round" x1={30} y1={55} x2={5} y2={30} strokeWidth={5} stroke="#fff" />
+            <line strokeLinecap="round" x1={30} y1={5} x2={5} y2={30} strokeWidth={5} stroke="#fff" />
+          </svg>
+        </div>
         <Navbar.Brand href="/" className="d-flex align-items-center">
           <Image
             src="/ETS-logo.png"
