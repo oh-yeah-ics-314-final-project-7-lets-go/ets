6bb5a3dc1671cef8a6b6fe3bc7bc386ed0ad5734
kyler <143767857+kylersm@users.noreply.github.com>
2025-12-03 11:52:49 -1000

    Add a prompt for deleting a user

--- src/components/admin/DeleteUserForm.tsx
+++ src/components/admin/DeleteUserForm.tsx
@@ -1,27 +1,59 @@
 'use client';
 
 import { deleteUser } from '@/lib/dbActions';
-import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { PersonDashFill } from 'react-bootstrap-icons';
 import swal from 'sweetalert';
 
-const DeleteUserForm = ({ id } : { id: number }) => (
-  <form
-    action={async () => {
-      await deleteUser(id);
-      await swal(
-        'Success',
-        'The user was deleted.',
-        'success',
-      );
-    }}
-  >
-    <Button variant="danger" size="sm" type="submit">
-      <PersonDashFill />
-      {' '}
-      Delete
-    </Button>
-  </form>
-);
+const DeleteUserForm = ({ id } : { id: number }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const handleDeleteCancel = () => {
+    setShowDeleteModal(false);
+  };
+
+  const handleDeleteConfirm = async () => {
+    await deleteUser(id);
+    await swal(
+      'Success',
+      'The user was deleted.',
+      'success',
+    );
+    setShowDeleteModal(false);
+  };
+
+  return (
+    <>
+      <Modal show={showDeleteModal} onHide={handleDeleteCancel} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="fw-bold text-danger">Are you sure you want to delete this user?</p>
+          <p className="text-muted">
+            This action cannot be undone.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleDeleteCancel}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDeleteConfirm}>
+            Delete User
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <form
+        action={() => setShowDeleteModal(true)}
+      >
+        <Button variant="danger" size="sm" type="submit">
+          <PersonDashFill />
+          {' '}
+          Delete
+        </Button>
+      </form>
+    </>
+  );
+};
 
 export default DeleteUserForm;
